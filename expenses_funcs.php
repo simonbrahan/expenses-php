80771cabf73e1665812759b4511292029897d5f3
Simon Brahan <simon.brahan@frogeducation.com>
2017-10-16 13:04:45 +0100

    Replaced fixtures with db calls

--- expenses_funcs.php
+++ expenses_funcs.php
@@ -1,58 +1,13 @@
 <?php
 function getLastTen()
 {
-    return array(
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-16')
-        ),
-        array(
-            'amount' => 4,
-            'description' => 'lunch thai',
-            'applied_on' => new \DateTime('2017-10-15')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-14')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-13')
-        ),
-        array(
-            'amount' => 2.5,
-            'description' => 'lunch pasta',
-            'applied_on' => new \DateTime('2017-10-13')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-12')
-        ),
-        array(
-            'amount' => 60,
-            'description' => 'car diesel',
-            'applied_on' => new \DateTime('2017-10-12')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-12')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-11')
-        ),
-        array(
-            'amount' => 1.5,
-            'description' => 'coffee',
-            'applied_on' => new \DateTime('2017-10-10')
-        )
+    $conn = getConn();
+
+    $res = $conn->query(
+        'select description, amount, applied_on from expense order by applied_on desc limit 10'
     );
+
+    return $res->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function groupByDate($expenses)
@@ -60,11 +15,11 @@ function groupByDate($expenses)
     $grouped_expenses = array();
 
     foreach ($expenses as $ungrouped_expense) {
-        if (!array_key_exists(dateToStr($ungrouped_expense['applied_on']), $grouped_expenses)) {
-            $grouped_expenses[dateToStr($ungrouped_expense['applied_on'])] = array();
+        if (!array_key_exists($ungrouped_expense['applied_on'], $grouped_expenses)) {
+            $grouped_expenses[$ungrouped_expense['applied_on']] = array();
         }
 
-        $grouped_expenses[dateToStr($ungrouped_expense['applied_on'])][] = $ungrouped_expense;
+        $grouped_expenses[$ungrouped_expense['applied_on']][] = $ungrouped_expense;
     }
 
     ksort($grouped_expenses);
@@ -74,28 +29,33 @@ function groupByDate($expenses)
 
 function getCategories()
 {
-    return array(
-        array(
-            'category' => 'car',
-            'total' => 60
-        ),
-        array(
-            'category' => 'coffee',
-            'total' => 10.5
-        ),
-        array(
-            'category' => 'lunch',
-            'total' => 6.5
-        )
+    $conn = getConn();
+
+    $res = $conn->query(
+        'select
+             lower(substring_index(description, " ", 1)) category,
+             sum(amount) as total
+         from expense
+         group by lower(substring_index(description, " ", 1))
+         order by total desc'
     );
+
+    return $res->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function add($description, $amount, $applied_on)
+function addExpense($description, $amount, $applied_on)
 {
-    return;
+    $conn = getConn();
+
+    $q = $conn->prepare(
+        'insert into expense(description, amount, applied_on)
+         values(?, ?, ?)'
+    );
+
+    $q->execute(array($description, $amount, $applied_on));
 }
 
-function dateToStr(\DateTime $date)
+function getConn()
 {
-    return $date->format('Y-m-d');
+    return new \PDO('mysql:dbname=frogos_simon_p1;', ' ', ' ');
 }
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 require 'expenses_funcs.php';
 
+if (isset($_POST['add_expense'])) {
+    addExpense(
+        $_POST['description'],
+        $_POST['amount'],
+        $_POST['applied_on']
+    );
+}
+
 require 'templates/index.php';
